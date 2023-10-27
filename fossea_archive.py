6d41517297416c3b76b02779e97f9874aab1882c
ratufacturing <149126430+ratufacturing@users.noreply.github.com>
2023-10-27 01:24:51 +0000

    Created repo
    Added files

---
+++ fossea_archive.py
@@ -0,0 +1,99 @@
+import lbrytools as lbryt
+import hashlib
+import time
+import fitz
+import re
+import os
+
+def main():
+    text = "0"
+    
+    #Download path selection and BLC list import
+    while True:
+        try:
+            ddir = input("Enter the absolute path to the download directory: ") 
+            #Check for valid input
+            if ddir == "" or not os.path.isdir(ddir):
+                continue
+
+            useBLC = input("Import the BLC Guncad list? ")
+            if useBLC == "y" or useBLC == "Y" or useBLC == "yes" or useBLC == "Yes":
+                #Download the current file and read in the text to the chosen directory
+                sourcePDF = lbryt.download_single(uri="@blacklotuscoalition:3/Odysee-list", ddir=ddir)
+                #Use textract to get the data from the pdf
+                readPDF = fitz.open(sourcePDF["download_path"])
+                for i in readPDF:
+                    text = text + i.get_text()
+                break
+
+            else:
+                print("Did not import the BLC list.")
+                break
+        except KeyboardInterrupt:
+            return
+        except:
+            print("Error, please try again:")
+            continue
+
+    #Supplementary file selection and loading
+    while True:
+        try:
+            
+            useSupp = input("Use a second file? ")
+            if useSupp == "y" or useSupp == "Y" or useSupp == "yes" or useSupp == "Yes":
+                suppFile = input("Enter the absolute path to the supplementary file: ")
+                text = text + open(suppFile,'r').read()
+            elif text == "0":
+                print("No inputs given, closing...")
+                return
+            else:
+                print("No secondary file chosen.")
+
+            skipVideo = input("Skip downloading video files? ")
+            if skipVideo == "y" or skipVideo == "Y" or useSupp == "yes" or useSupp == "Yes":
+                downloadVideos = False
+            else:
+                downloadVideos = True
+            break
+        except KeyboardInterrupt:
+            return
+        except:
+            print("Error, please try again:")
+            continue
+    
+    channels = re.findall(r"(@[^\\:;/]+)", text)
+
+    #Clean  channel list
+    for i in channels:
+        if "\\" in i or "https" in i:
+            channels.remove(i)
+
+    #Iterate through each channel
+    for i in channels:
+        print(f"Now downloading channel: {str(i)} \n")
+        #Find all claims of the channel
+        claimIds = []
+        channelClaims = lbryt.list_ch_claims(i, claim_id=True, title=True)
+        for i in channelClaims['claims']:
+
+            #If it's a report, ignore 
+            if i['value_type'] != 'repost':
+
+                if downloadVideos == True:
+                    claimIds.append(i["claim_id"])
+                
+                elif downloadVideos == False:
+                    if i['value']['stream_type'] != 'video':
+                        claimIds.append(i["claim_id"])
+
+        #Iterate through each claim we've chosen to download, and pass them to the lbrynet daemon
+        for i in claimIds:
+            try:
+                lbryt.download_single(cid=i, ddir=ddir, own_dir=True)
+            except KeyboardInterrupt:
+                return
+            except:
+                pass
+
+if __name__ == "__main__":
+    main()
---
+++ readme.txt
@@ -0,0 +1,77 @@
+----------------------------------------------Instructions for usage----------------------------------------------
+
+
+					Windows 10
+
+1. Install Python and Git
+Install Python:
+https://www.python.org/ftp/python/3.12.0/python-3.12.0-amd64.exe
+Make sure the Python executable is added to the PATH so that it can be launched from anywhere in your system.
+
+Install Git:
+https://gitforwindows.org/
+Make sure to check "Git from the command line" in PATH settings during installation
+
+2. Install dependancies
+#This will install the python packages you need
+python -m pip install requests regex PyMuPDF
+
+#Run the following line, and copy the output:
+python -m site --user-site
+cd %Output of above%
+#Into here ^, remove the %'s
+mkdir lbrytools
+git clone https://github.com/belikor/lbrytools/
+
+3. Download and start lbrynet
+#Download and extract the latest lbrynet release from here: https://github.com/lbryio/lbry-sdk/releases
+#Then run it by opening a command prompt at this folder (you can type CMD into folder path in file explorer to open a terminal, or use cd to navigate to the directory) and run: 
+lbrynet start
+
+4. Run the Python file
+#Download the python file from the Github, and open a terminal in the folder you download it to. Run it with
+python fossea_archive.py
+#Enter the download directory- where you want your files to go. For example:
+C:\Users\%Username%\Documents\LBRYDL
+#Choose whether you want to import the BLC Guncad list, answers other than yes/y will default to no
+#Choose if you want to use a supplementary file (should probably be .txt). This should recognize any @username as long as they're seperated on different lines or with semicolons or similar.
+#Make sure you give it the absolute path, something like:
+C:/Users/%Username%/Documents/LBRYDL/extra_channels.txt
+
+
+
+					Ubuntu:
+
+1. Install Python and Git
+Should both already by installed, but if not:
+sudo apt update
+sudo apt install git
+sudo apt install python3
+
+2. Install dependancies
+#This will install the python packages you need
+pip install requests regex PyMuPDF
+
+#Run the following line, and copy the output:
+python3 -m site --user-site
+cd %Output of above%
+#Into here ^, remove the %'s
+mkdir lbrytools
+git clone https://github.com/belikor/lbrytools/
+
+3. Download and start lbrynet
+#Download and extract the latest lbrynet appimage release from here: https://github.com/lbryio/lbry-sdk/releases
+#Then navigate to your extracted folder and run: 
+./lbrynet start
+#If you're having trouble with wallet things and are having connection issues, it's worth checking to see if it's a hashlib error- I was having issues but fixed it with this: https://stackoverflow.com/a/72509045
+
+4. Run the Python file
+#Download the python file from the Github, and open a terminal in the folder you download it to. Run it with:
+python3 fossea_archive.py
+#Enter the download directory- where you want your files to go. For example:
+/home/%Username%/LBRYDL
+#Choose whether you want to import the BLC Guncad list, answers other than yes/y will default to no
+#Choose if you want to use a supplementary file (should probably be .txt). This should recognize any @username as long as they're seperated on different lines or with semicolons or similar.
+#Make sure you give it the absolute path, something like:
+/home/%Username%/LBRYDL/extra_channels.txt
+
